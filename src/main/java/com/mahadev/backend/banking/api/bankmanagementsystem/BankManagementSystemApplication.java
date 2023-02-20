919fa25da58e28751f6398d21bc76246f004e921
MAHADEV CHAURASIYA <mdchaurasiya@gmail.com>
2023-02-20 16:26:26 +0530

    Bank management system api

---
+++ src/main/java/com/mahadev/backend/banking/api/bankmanagementsystem/BankManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.mahadev.backend.banking.api.bankmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MAHADEV CHAURASIYA
+ * <a href="https://www.linkedin.com/in/mahadev-chaurasiya/">MAHADEV CHAURASIYA</a>
+ */
+@SpringBootApplication
+public class BankManagementSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahadev/backend/banking/api/bankmanagementsystem/BankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahadev.backend.banking.api.bankmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
